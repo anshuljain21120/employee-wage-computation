6af26fdc83fb8dc7e88ea1c6af34948e3f219b1c
Anshul Jain <anshuljain21120@gmail.com>
2020-07-14 12:22:07 +0530

    add attendance check

--- calculate-employee-wage.sh
+++ calculate-employee-wage.sh
@@ -1,2 +1,16 @@
 #! /bin/bash -x
 echo "Welcome to Employee wage computation program!";
+
+function employee_attendance()
+{
+	local present_mark absent_mark randomCheck;
+
+	present_mark="${1:-1}";
+	absent_mark="${2:-0}";
+	randomCheck=$((RANDOM%2));
+
+	( [ $randomCheck -eq 1 ] && echo "$present_mark") || echo "$absent_mark";
+}
+
+echo "$( employee_attendance 'Employee is present.' 'Employee is absent.' )";
+
